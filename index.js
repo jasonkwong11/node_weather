be9ce109f7eaefa90a4fb7a6e13f8eceae7d525a
Jason <jasonkwong11@gmail.com>
2017-02-12 07:23:44 -0500

    progress on cli interface

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
-const express = require('express')
-const rp = require('request-promise')
-const exphbs = require('express-handlebars')
-const path = require('path')
+var Client = require('node-rest-client').Client;
+const client = new Client();
 
-const app = express()
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+var util = require('util');
+ 
 
-app.engine('.hbs', exphbs({
-  defaultLayout: 'main',
-  extname: '.hbs',
-  layoutsDir: path.join(__dirname, 'views/layouts')
-}))
+var apiKey = 'lszGWElRufBBbuGWwtJc95DPfp0NGXH5'
 
-app.set('view engine', '.hbs')
-app.set('views', path.join(__dirname, 'views'))
+const initialize = () => {
+  console.log('Checking the weather? Please enter a city, zipcode, or quit.')
 
-window.alert('HI')
-//GET the location key from Accuweather API
-app.get('/:city', (req, res) => {
-  rp({
-    uri: 'http://dataservice.accuweather.com/locations/v1/search',
-    qs: {
-      q: req.params.city,
-      apiKey: 'lszGWElRufBBbuGWwtJc95DPfp0NGXH5',
-      language: 'en-us',
-      details: true,
-      offset: 0,
-      alias: 'NoOfficialMatchFound'
-    },
-    json: true
+  process.stdin.on('data', function (text){
+
+    if (text === '\n'){
+      console.log('Que? Please enter a city or zipcode or quit.')
+      return initialize();
+    }
+
+    console.log('Here is the weather for ', util.inspect(text).substr(1, text.length-1));
+    if (text === 'quit\n'){
+      done();
+    }
+
+    //GET the location key from Accuweather API
+    client.get('http://dataservice.accuweather.com//locations/v1/search?apikey=' + apiKey + '&q=' + text, (data, response) => {
+      console.log(`We found the ${data.length} matches for your query...`);
+
+      if (data.length === 0){
+        client.removeAllListeners()
+        console.log('No matches found. Please try again')
+        return initialize();
+      }
+
+      if (data.length > 10){
+        client.removeAllListeners()
+        console.log('Please be more specific')
+        return initialize();
+      } 
+      data.forEach((city) => {console.log(city.EnglishName + ', ' + city.AdministrativeArea.LocalizedName + ', ' + city.AdministrativeArea.CountryID)})
+    })
   })
-    .then((data) => {
-      res.render('index', data)
-    })
-    .catch((err) => {
-      console.log(err)
-      res.render('error')
-    })
-})
+}
 
-app.listen(3000)
-
+initialize()
